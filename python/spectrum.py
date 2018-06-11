179e0de2a44d8a7566e6f2606e8eac00723fd992
DouglasLeeTucker <dtucker@fnal.gov>
2018-06-11 16:55:40 -0500

    Added code for synphot-like input spectrum files

--- python/spectrum.py
+++ python/spectrum.py
@@ -16,3 +16,35 @@ def getSpectrumFlat(flux=1.0):
                              kind='linear')
 
     return f
+
+def getSpectrumSynphot(synphotFileName, fluxFactor=1.0):
+
+    import numpy as np
+    from scipy import interpolate 
+    from astropy.io import fits
+    from astropy.table import Table
+    import sys
+
+    try:
+
+        hdulist = fits.open(synphotFileName)
+        t = Table.read(hdulist[1])
+        hdulist.close()
+
+    except IOError:
+
+        print """Could not read %s""" % synphotFileName
+        sys.exit(1)
+
+
+    wave = t['WAVELENGTH'].data.tolist()
+    #flam = fluxFactor*t['FLUX'].data.tolist()   
+    flam = t['FLUX'].data.tolist()   
+    data = {'wavelength': wave, 'flux': flam}
+
+    f = interpolate.interp1d(data['wavelength'], data['flux'], 
+                             bounds_error=False, fill_value=0., 
+                             kind='linear')
+
+    return f
+
