0ee0152605df2d85dc9b347a8ee64f15e3e09d0c
DouglasLeeTucker <dtucker@fnal.gov>
2018-11-01 14:11:31 -0500

    Slighty relaxed constraints on flags and imaflags_iso in se_object query.

--- bin/rawdata_se_object_query.py
+++ bin/rawdata_se_object_query.py
@@ -54,6 +54,51 @@ def rawdata_se_object_query(args):
 
     # Based on query from Eli Rykoff (9 Oct 2017), 
     #  and input from days of old from Robert Gruendl...
+    #query="""
+    #with x as (
+    #    select /*+materialize */ pfw_attempt_id
+    # 	from prod.proctag
+	#where tag='%s'
+	#),
+    #y as (
+    #    select /*+materialize */ f.expnum, f.pfw_attempt_id
+	#from prod.finalcut_eval f, x
+	#where f.pfw_attempt_id = x.pfw_attempt_id
+	#),
+    #z as (
+    #    select /*+materialize */ e.expnum,
+    #        cast(e.band as VARCHAR(1)) as BAND,
+    #        c.filename,c.ccdnum
+    # 	from prod.catalog c, prod.exposure e, y
+	#where c.expnum=y.expnum
+	#    and e.expnum=c.expnum
+	#    and c.pfw_attempt_id=y.pfw_attempt_id
+	#    and c.filetype='cat_finalcut'
+        #    and e.expnum between %d and %d
+    	#)
+    #select cast(z.expnum as NUMBER(8)) as EXPNUM,
+    #       z.filename, z.band, 
+    #       cast(z.ccdnum as NUMBER(4)) as CCDNUM,
+    #       o.object_number, o.x_image, o.y_image, o.ra, o.dec, 
+    #       o.flux_psf, o.fluxerr_psf, o.flux_aper_8, fluxerr_aper_8,
+    #       o.class_star, o.spread_model, o.spreaderr_model, o.flags
+    #from prod.se_object o, z
+    #where o.filename=z.filename
+    #    and (o.x_image between 100 and 1900)
+    #    and (o.y_image between 100 and 3950)
+    #    and o.flags = 0
+    #    and o.imaflags_iso = 0
+    #    and (o.spread_model between -0.01 and 0.01)
+    #    and o.class_star > 0.80
+    #    and ((1.086*o.fluxerr_psf/o.flux_psf) between 0.0001 and 0.1000)
+    #    """ % \
+    #    (args.tag, args.expnumMin, args.expnumMax)
+
+    # Based on query from Eli Rykoff (9 Oct 2017), 
+    #  and input from days of old from Robert Gruendl...
+    # 20180629:  changed "o.flags = 0" and "o.imaflags_iso = 0"
+    #            to "o.flags < 2" and "o.imaflags_iso < 2"
+
     query="""
     with x as (
         select /*+materialize */ pfw_attempt_id
@@ -89,8 +134,8 @@ def rawdata_se_object_query(args):
     where o.filename=z.filename
         and (o.x_image between 100 and 1900)
         and (o.y_image between 100 and 3950)
-        and o.flags = 0
-        and o.imaflags_iso = 0
+        and o.flags < 2
+        and o.imaflags_iso < 2
         and (o.spread_model between -0.01 and 0.01)
         and o.class_star > 0.80
         and ((1.086*o.fluxerr_psf/o.flux_psf) between 0.0001 and 0.1000)
