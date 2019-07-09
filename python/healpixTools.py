9b25d755adb886da6129b55f6162a37308db95be
DouglasLeeTucker <dtucker@fnal.gov>
2019-07-09 18:27:34 -0500

    Added nest=True/False option to getipix

--- python/healpixTools.py
+++ python/healpixTools.py
@@ -7,11 +7,12 @@ def radec2thetaphi(ra, dec):
     return (90-dec)*np.pi/180., ra*np.pi/180.
 
 ##################################
-#DESDM uses nside=128
-def getipix(nside,ra,dec):
+#DESDM uses nside=128, nest=True
+#Alex Drlica Wagner's healpixelated Gaia DR2 on des40 uses nside=32, nest=False
+def getipix(nside,ra,dec,nest=True):
     import healpy as hp
     theta, phi = radec2thetaphi(ra, dec)
-    ipix = hp.pixelfunc.ang2pix(nside, theta, phi, nest=True)
+    ipix = hp.pixelfunc.ang2pix(nside, theta, phi, nest)
     return ipix
 
 ##################################
